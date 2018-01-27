4d24814aef5eb25d88dfdcec087252e77345c592
Eric Rabil <ericjrabil@gmail.com>
2018-01-27 12:40:51 -0500

    Disable exception handling

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -4,7 +4,7 @@ export const logger = new winston.Logger({
     transports: [
         new winston.transports.Console({
             level: "debug",
-            handleExceptions: true,
+            handleExceptions: false,
             json: false,
             colorize: true,
         }),
