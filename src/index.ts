8e29db5940e58d537e52f5471e6e4236746fba2e
Eric Rabil <ericjrabil@gmail.com>
2018-01-27 12:46:42 -0500

    Here's a basic start script, starts 2/3 services

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
+import GatewayClient from "./bridge";
 import config from "./config";
+import { connect } from "./db/connection";
 
-console.log(config.bridge.port);
+const bridgeURI: string = config.bridge.address + (config.bridge.port ? `:${config.bridge.port}` : "") + (config.bridge.path ? config.bridge.path : "");
+
+console.log(bridgeURI);
+
+const client = new GatewayClient(bridgeURI);
+client.start();
+
+client.on("open", async () => {
+    await connect();
+});
