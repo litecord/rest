fee44f310f4687f30f4765afb0d2f3567521921c
Eric Rabil <ericjrabil@gmail.com>
2018-01-27 12:41:48 -0500

    Added snowflake generation utilities

---
+++ src/util/snowflakeUtils.ts
@@ -0,0 +1,19 @@
+let INCREMENT: number = 0;
+const EPOCH: number = 1514764800;
+
+import * as Long from "long";
+
+function pad(v: any, n: any, c: any = "0") {
+    return String(v).length >= n ? String(v) : (String(c).repeat(n) + v).slice(-n);
+}
+
+export function generateSnowflake(time = Date.now()) {
+    if (INCREMENT >= 4095) {
+        INCREMENT = 0;
+    }
+    if (isNaN((new Date(time)).getTime()) && typeof time === "number") {
+        throw new Error(`Invalid timestamp`);
+    }
+    const BINARY = `${pad((time - EPOCH).toString(2), 42)}0000100000${pad((INCREMENT++).toString(2), 12)}`;
+    return Long.fromString(BINARY, 2).toString();
+}
