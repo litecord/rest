0b57a2a2b37fce8d4273c35d9783f3db2c09d67f
Eric Rabil <ericjrabil@gmail.com>
2018-01-27 23:03:04 -0500

    Document snowflake utils

--- src/util/snowflakeUtils.ts
+++ src/util/snowflakeUtils.ts
@@ -7,6 +7,11 @@ function pad(v: any, n: any, c: any = "0") {
     return String(v).length >= n ? String(v) : (String(c).repeat(n) + v).slice(-n);
 }
 
+/**
+ * Generates a Discord-like snowflake. Very basic.
+ *
+ * @param time the time this snowflake is for
+ */
 export function generateSnowflake(time = Date.now()) {
     if (INCREMENT >= 4095) {
         INCREMENT = 0;
