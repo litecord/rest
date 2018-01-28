7dbd513ba1ae368039b9244478e57d31bd416c02
Eric Rabil <ericjrabil@gmail.com>
2018-01-27 23:03:10 -0500

    Run the REST server

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import GatewayClient from "./bridge";
 import config from "./config";
 import { connect } from "./db/connection";
+import { DiscordExpress } from "./rest/server";
 
 const bridgeURI: string = config.bridge.address + (config.bridge.port ? `:${config.bridge.port}` : "") + (config.bridge.path ? config.bridge.path : "");
-
-console.log(bridgeURI);
 
 const client = new GatewayClient(bridgeURI);
 client.start();
 
 client.on("open", async () => {
     await connect();
+    const express = new DiscordExpress(client);
+    express.bridge = express.bridge;
 });
