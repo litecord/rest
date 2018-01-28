ff4d711c119dd4506f4fe6dbba40bb34d32db4d7
Eric Rabil <ericjrabil@gmail.com>
2018-01-27 22:57:50 -0500

    Added convenience methods for discrim rolling, password generation and token generation

--- src/db/entities/User.ts
+++ src/db/entities/User.ts
@@ -1,7 +1,9 @@
+import * as bcrypt from "bcrypt";
 import * as nobi from "nobi";
-import {Entity, PrimaryGeneratedColumn, Column, BaseEntity, PrimaryColumn} from "typeorm";
+import {Entity, Column, BaseEntity, PrimaryColumn} from "typeorm";
 import { generateSnowflake } from "../../util/snowflakeUtils";
 import { nonce } from "../../util/index";
+import { createToken } from "../../util/hashingUtils";
 
 @Entity()
 class Users extends BaseEntity {
@@ -41,6 +43,34 @@ class Users extends BaseEntity {
 
     @Column("text")
     public password_salt: string = nobi(nonce()).sign(Date.now() + "");
+
+    public async newDiscriminator(): Promise<void> {
+        const rollTheDice = async (): Promise<void> => {
+            const discrim = Math.floor(Math.random() * 9999) + "";
+            const docs = await Users.findOne({username: this.username, discriminator: discrim});
+            if (docs) {
+                return rollTheDice();
+            } else {
+                this.discriminator = discrim;
+            }
+        };
+        return rollTheDice();
+    }
+
+    public async setPassword(password: string): Promise<void> {
+        const salt = this.password_salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+        this.password_hash = hash;
+        return;
+    }
+
+    public async comparePasswords(password: string): Promise<boolean> {
+        return bcrypt.compare(password, this.password_hash);
+    }
+
+    public async generateToken(): Promise<string> {
+        return await createToken(this);
+    }
 
 }
 
